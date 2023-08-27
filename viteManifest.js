d158295b17cabaa26268552bfe74de1d4d404934
Davidvn95 <davidvergaraok@gmail.com>
2023-08-26 22:43:07 -0500

    corrigiendo Manifest icons

--- viteManifest.js
+++ viteManifest.js
@@ -9,17 +9,23 @@ export const manifestForPlugin = {
         description: 'Una app de juego de memoria dedáctico para niños',
         "icons": [
             {
-                "src": './src/assets/images/maskable_icon.png',
-                "sizes": '1280x1280',
+                "src": './src/assets/images/maskable_icon_x512.png',
+                "sizes": '144x144',
                 "type": 'image/png',
-                "purpose": 'maskable',
+                "purpose": 'any',
             },
             {
                 "src": './src/assets/images/maskable_icon_x512.png',
                 "sizes": '512x512',
                 "type": 'image/png',
                 "purpose": 'maskable',
-            }
+            },
+            {
+                "src": './src/assets/images/maskable_icon.png',
+                "sizes": '1280x1280',
+                "type": 'image/png',
+                "purpose": 'maskable',
+            },
         ],
         theme_color: '#ffffff',
         background_color: '#84CC16',
