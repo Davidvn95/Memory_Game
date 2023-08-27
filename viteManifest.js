5b16897383de1a09f9f129f1d17a0a3c72839c1a
Davidvn95 <davidvergaraok@gmail.com>
2023-08-26 20:27:16 -0500

    corrigiendo Manifest

--- viteManifest.js
+++ viteManifest.js
@@ -5,24 +5,24 @@ export const manifestForPlugin = {
         name: 'Memory Game',
         short_name: 'MemoryGame',
         description: 'Una app de juego de memoria dedáctico para niños',
-        icons: [
+        "icons": [
             {
-                src: './src/assets/images/MemoryGameLogo.jpeg',
-                sizes: '512x512',
-                type: 'image/jpeg',
-                purpose: 'any',
+                "src": './src/assets/images/MemoryGameLogo.jpeg',
+                "sizes": '512x512',
+                "type": 'image/jpeg',
+                "purpose": 'any',
             },
             {
-                src: './src/assets/images/maskable_icon.png',
-                sizes: '1280x1280',
-                type: 'image/png',
-                purpose: 'maskable',
+                "src": './src/assets/images/maskable_icon.png',
+                "sizes": '1280x1280',
+                "type": 'image/png',
+                "purpose": 'maskable',
             },
             {
-                src: './src/assets/images/maskable_icon_x512.png',
-                sizes: '512x512',
-                type: 'image/png',
-                purpose: 'maskable',
+                "src": './src/assets/images/maskable_icon_x512.png',
+                "sizes": '512x512',
+                "type": 'image/png',
+                "purpose": 'maskable',
             }
         ],
         theme_color: '#ffffff',
